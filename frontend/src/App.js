a691ef82dc49316092bba89de9740e50e4a419a3
David <david.pok@gmail.com>
2021-08-03 15:24:01 -0700

    adds react app and dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+        Hello world!
+    </div>
+  );
+}
+
+export default App;
