911f1f645ab15f557802627a0ed9e5bca31fb014
David <david.pok@gmail.com>
2021-08-03 15:35:23 -0700

    adds navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,53 @@
+import React, { useState } from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddReview from "./components/addReview";
+import Login from "./components/login";
+import Rests from "./components/rests";
+import RestsLists from "./components/restsLists";
+
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div className="App">
-        Hello world!
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item">
+            {user ? (
+              <a
+                onClick={logout}
+                className="nav-link"
+                style={{ cursor: "pointer" }}
+              >
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
     </div>
   );
 }
